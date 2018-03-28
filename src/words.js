523d2abe9ad8b3548fea5be81a85074dc07d7f31
tiiarautavesi <tiia.rautavesi@metropolia.fi>
2018-03-28 23:34:22 +0300

    Add words
    Search words for the game,
    One object in Finnish
    One object in Dutch

---
+++ src/words.js
@@ -0,0 +1,107 @@
+/* js object that provides the finnish words that will later be randomized and used as search words 
+var words = {
+  "Home": { // category
+    wd1: "pöytä",
+    wd2: "keittiö",
+    wd3: "jääkaappi",
+    wd4: "tuoli",
+    wd5: "sohva",
+    wd6: "kaappi"
+  },
+  "People": {
+    wd1: "nainen",
+    wd2: "mies",
+    wd3: "lapsi",
+    wd4: "perhe",
+    wd5: "vauva",
+    wd6: "vanhus"
+  },
+  "Devices": {
+    wd1: "lentokone",
+    wd2: "auto",
+    wd3: "puhelin",
+    wd4: "tietokone",
+    wd5: "juna",
+    wd6: "raitiovaunu"
+  },
+  "Food": {
+    wd1: "liha",
+    wd2: "peruna",
+    wd3: "tomaatti",
+    wd4: "banaani",
+    wd5: "omena",
+    wd6: "appelsiini"
+  },
+  "Clothes": {
+    wd1: "paita",
+    wd2: "housut",
+    wd3: "kengät",
+    wd4: "sukat",
+    wd5: "takki",
+    wd6: "hattu"
+  },
+  "Random": {
+    wd1: "",
+    wd2: "",
+    wd3: "",
+    wd4: "",
+    wd5: "",
+    wd6: ""
+  }
+};
+
+*/
+
+/* for dutch translations 
+
+var DutchWords = {
+  "home": { // category
+    wd1: "tafel",
+    wd2: "keuken",
+    wd3: "koelkast",
+    wd4: "stoel",
+    wd5: "bank",
+    wd6: "kast"
+  },
+  "people": {
+    wd1: "vrouw",
+    wd2: "man",
+    wd3: "kind",
+    wd4: "familie",
+    wd5: "baby",
+    wd6: "bejaarde"
+  },
+  "devices": {
+    wd1: "vliegtuig",
+    wd2: "auto",
+    wd3: "telefoon",
+    wd4: "computer",
+    wd5: "trein",
+    wd6: "tram"
+  },
+  "food": {
+    wd1: "vlees",
+    wd2: "aardappel",
+    wd3: "tomaat",
+    wd4: "banaan",
+    wd5: "appel",
+    wd6: "sinaasappel"
+  },
+  "clothes": {
+    wd1: "shirt",
+    wd2: "broek",
+    wd3: "schoenen",
+    wd4: "sok",
+    wd5: "jas",
+    wd6: "hooftdeksel"
+  },
+  "random": {
+    wd1: "",
+    wd2: "",
+    wd3: "",
+    wd4: "",
+    wd5: "",
+    wd6: ""
+  }
+};
+*/
